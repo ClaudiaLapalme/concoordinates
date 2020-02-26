a2b4e194e347f81d7cae18a163a7c11ff6beba31
david-ron <ronci.david@gmail.com>
2020-02-26 18:38:21 -0500

    #58 added route factory

---
+++ src/app/core/services/route-factory.ts
@@ -0,0 +1,44 @@
+import { Time } from "@angular/common";
+import { Injectable } from "@angular/core";
+import { DirectionsRequest } from "@google/maps";
+import { start } from "repl";
+import { throwError } from "rxjs";
+import { Route } from "../models/route";
+import { RoutesService } from "./routes.service";
+
+@Injectable()
+export class RouteFactory {
+  constructor(private routesService: RoutesService) {}
+
+  generateAccesibleRoutes = (
+    startCoordinates: Coordinates,
+    endCoordinates: Coordinates,
+    startTime: Time,
+    endTime: Time
+  ) => {
+    return null;
+  };
+
+  generateDefaultRoutes(
+    startCoordinates: number,
+    endCoordinates: number,
+    startTime?: Date,
+    endTime?: Date,
+    transitMode?: any
+  ): Route[] {
+    if (transitMode !== undefined) {
+      if (!transitMode[typeof google.maps.TransitMode]) {
+        throw new Error("Invalid Transitmode type used");
+      }
+    }
+    const dirRequest: google.maps.DirectionsRequest = {
+      destination: new google.maps.LatLng(startCoordinates, endCoordinates),
+      travelMode: transitMode,
+      transitOptions: { departureTime: startTime, arrivalTime: endTime },
+      provideRouteAlternatives: true
+    };
+    this.routesService.getRoutes(dirRequest);
+    const tmp = [new Route()] as Route;
+    return tmp;
+  }
+}
