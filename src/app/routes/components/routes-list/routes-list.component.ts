1be8dac9c9df213bcd3a25bd62f2277c37966448
david-ron <ronci.david@gmail.com>
2020-03-09 14:36:40 -0400

    logging the route and making it clickable from list

--- src/app/routes/components/routes-list/routes-list.component.ts
+++ src/app/routes/components/routes-list/routes-list.component.ts
@@ -1,15 +1,21 @@
-import { Component, Input, OnInit  } from '@angular/core';
-import { Route, TransportMode  } from 'src/app/core';
+import { Component, Input, OnInit } from "@angular/core";
+import { Route, TransportMode } from "src/app/core";
 
 @Component({
-    selector: 'app-routes-list',
-    templateUrl: './routes-list.component.html',
-    styleUrls: ['./routes-list.component.scss']
+  selector: "app-routes-list",
+  templateUrl: "./routes-list.component.html",
+  styleUrls: ["./routes-list.component.scss"]
 })
 export class RoutesListComponent implements OnInit {
-    @Input() routes: Route[];
-    @Input() routeTransportMode: TransportMode;
-    constructor() {}
+  @Input() routes: Route[];
+  @Input() routeTransportMode: TransportMode;
+  constructor() {}
 
-    ngOnInit() {}
+  ngOnInit() {}
+  displayRoute(i: number) {
+    const selectedRoute = this.routes[i];
+    console.log(selectedRoute);
+
+    // redirect to other page and send route along with it
+  }
 }
