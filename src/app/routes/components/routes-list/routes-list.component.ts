2352e555eb31d88a69368e5dbfca6c7b98ea8dd3
david-ron <ronci.david@gmail.com>
2020-03-15 15:48:45 -0400

    change in route-list to get transportmode used in UI for rendering directions

--- src/app/routes/components/routes-list/routes-list.component.ts
+++ src/app/routes/components/routes-list/routes-list.component.ts
@@ -4,18 +4,23 @@ import { Route, TransportMode } from 'src/app/core';
 import { StateService } from 'src/app/shared/state.service';
 
 @Component({
-  selector: 'app-routes-list',
-  templateUrl: './routes-list.component.html',
-  styleUrls: ['./routes-list.component.scss']
+    selector: 'app-routes-list',
+    templateUrl: './routes-list.component.html',
+    styleUrls: ['./routes-list.component.scss']
 })
 export class RoutesListComponent implements OnInit {
-  @Input() routes: Route[];
-  @Input() routeTransportMode: TransportMode;
-  constructor(private router: Router, private stateService: StateService) { }
+    @Input() routes: Route[];
+    @Input() routeTransportMode: TransportMode;
+    constructor(private router: Router, private stateService: StateService) {}
 
-  ngOnInit() { }
-  displayRoute(i: number) {
-    this.stateService.sharedRoute = this.routes[i];;
-    this.router.navigateByUrl('rendered-routes');
-  }
+    ngOnInit() {}
+    displayRoute(i: number) {
+        this.stateService.sharedRoute = this.routes[i];
+
+        // TODO get Transport Object
+        this.stateService.sharedRoute.setCurrentTravelMode(
+            this.routeTransportMode
+        );
+        this.router.navigateByUrl('rendered-routes');
+    }
 }
