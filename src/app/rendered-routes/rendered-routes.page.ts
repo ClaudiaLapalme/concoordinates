57dfbdf3cc3bbe008128e1e2b46f823db94ccf97
david-ron <ronci.david@gmail.com>
2020-03-15 13:43:42 -0400

    additions elements for tests in route and rendered-route and change to transport-mode attribute for name consistency

--- src/app/rendered-routes/rendered-routes.page.ts
+++ src/app/rendered-routes/rendered-routes.page.ts
@@ -1,4 +1,10 @@
-import { AfterViewInit, Component, ElementRef,  ViewChild, OnInit } from '@angular/core';
+import {
+    AfterViewInit,
+    Component,
+    ElementRef,
+    ViewChild,
+    OnInit
+} from '@angular/core';
 import { Route } from '../core/models';
 import { MapService } from '../core/services';
 import { StateService } from '../shared/state.service';
@@ -6,23 +12,22 @@ import { StateService } from '../shared/state.service';
 @Component({
     selector: 'app-rendered-routes',
     templateUrl: './rendered-routes.page.html',
-    styleUrls: ['./rendered-routes.page.scss'],
+    styleUrls: ['./rendered-routes.page.scss']
 })
 export class RenderedRoutesPage implements AfterViewInit, OnInit {
-
     route: Route;
     @ViewChild('map', { static: false })
     mapElement: ElementRef;
-    constructor(private stateService: StateService) { }
+    constructor(private stateService: StateService) {}
 
     ngAfterViewInit(): void {
         const map = new google.maps.Map(this.mapElement.nativeElement);
-        console.log(this.route);
-        this.route.display(map);
+        const renderer = new google.maps.DirectionsRenderer();
+        renderer.setMap(map);
+        this.route.display(renderer);
     }
 
     ngOnInit() {
         this.route = this.stateService.sharedRoute;
     }
-
 }
--- src/test-mocks/maps.googleapis.com-maps-api.js
+++ src/test-mocks/maps.googleapis.com-maps-api.js
@@ -3,94 +3,128 @@
  * Please don't delete this file.
  */
 
-class Polygon{
+class Polygon {
+    visible = false;
 
-	visible = false;
-
-	setMap(){};
-	setVisible(value){
-		this.visible = value;
-	};
-	getVisible(){
-		return this.visible;
-	};
-	addListener(){};
+    setMap() {}
+    setVisible(value) {
+        this.visible = value;
+    }
+    getVisible() {
+        return this.visible;
+    }
+    addListener() {}
 }
 
-class Marker{
+class Marker {
+    visible = false;
 
-	visible = false;
-
-	setMap(){};
-	setVisible(value){
-	   this.visible = value;
-	};
-	getVisible(){
-		return this.visible;
-	};
+    setMap() {}
+    setVisible(value) {
+        this.visible = value;
+    }
+    getVisible() {
+        return this.visible;
+    }
 }
 
-class DirectionsService{
-	route(dirRequest, res, status){};
+class DirectionsService {
+    route(dirRequest, res, status) {}
 }
 
-class LatLngBounds{
-    extend(){};
-    getCenter(){};
+class LatLngBounds {
+    extend() {}
+    getCenter() {}
+    setMap() {}
+    setVisible(value) {
+        this.visible = value;
+    }
+    getVisible() {
+        return this.visible;
+    }
 }
-
+class DirectionsService {
+    route(request, callback) {}
+}
+class DirectionsRenderer {
+    setMap(map) {}
+}
 let google = {
-    maps : {
-        OverlayView : function () {
-			return {
-				setMap : function () {
-				},
-			}
+    maps: {
+        OverlayView: function() {
+            return {
+                setMap: function() {}
+            };
         },
-        Marker : function () {
-			return new Marker;
+        Marker: function() {
+            return new Marker();
         },
-        InfoWindow : function () {
-		},
-		LatLng: function(lat, lng){
-        	return [lat, lng];
-		},
-		LatLngBounds: function(sw, ne) {
-            return new LatLngBounds
+        InfoWindow: function() {},
+        LatLng: function(lat, lng) {
+            return [lat, lng];
         },
-        Map: function(obj){
-
-		},
-		Size: function(){
-
-		},
-		Polygon: function(){
-			return new Polygon;
-		},
-		DirectionsService: function(){
-			return new DirectionsService;
-		},
-        MapTypeId: {ROADMAP: true},
+        LatLngBounds: function(sw, ne) {
+            return new LatLngBounds();
+        },
+        Map: function(obj) {},
+        Size: function() {},
+        Polygon: function() {
+            return new Polygon();
+        },
+        DirectionsService: function() {
+            return new DirectionsService();
+        },
+        MapTypeId: { ROADMAP: true },
         places: {
-        	AutocompleteService: function(){
-
-        	},
-    		PlacesService: function(obj){
-    			return {
-    				PlacesServiceStatus: {
-	        			OK: true
-	        		},
-	        		textSearch: function(query){
-	        			return [];
-	        		},
-	        		nearbySearch: function(query){
-	        			return [];
-					},
-					getDetails: function(){
-						return ['details'];
-					}
-    			};	
-    		}
+            AutocompleteService: function() {},
+            PlacesService: function(obj) {
+                return {
+                    PlacesServiceStatus: {
+                        OK: true
+                    },
+                    textSearch: function(query) {
+                        return [];
+                    },
+                    nearbySearch: function(query) {
+                        return [];
+                    },
+                    getDetails: function() {
+                        return ['details'];
+                    }
+                };
+            }
+        },
+        Marker: function() {},
+        InfoWindow: function() {},
+        LatLng: function(lat, lng) {
+            return [lat, lng];
+        },
+        LatLngBounds: function(sw, ne) {
+            return null;
+        },
+        Map: function(obj) {},
+        Size: function() {},
+        Polygon: function() {
+            return new Polygon();
+        },
+        MapTypeId: { ROADMAP: true },
+        DirectionsRenderer: function() {
+            return new DirectionsRenderer();
+        },
+        DirectionsRequest: function(
+            destination,
+            origin,
+            provideRouteAlternatives,
+            transitOptions,
+            travelMode
+        ) {
+            return new DirectionsRequest(
+                destination,
+                origin,
+                provideRouteAlternatives,
+                transitOptions,
+                travelMode
+            );
         }
     }
 };
