1bd1c629e0fd06e9d87826cf9bf4f137638e5121
JonathanHypex <jonathan@hypex.gg>
2020-03-20 15:01:09 -0400

    Fixed formatting and added missing return types to some functions

--- src/app/core/services/calendar.service.spec.ts
+++ src/app/core/services/calendar.service.spec.ts
@@ -4,45 +4,41 @@ import { CalendarService } from './calendar.service';
 
 describe('CalendarService', () => {
 
-  window['gapi'] = {
-    load() {
-      return null;
-    }}
+    window['gapi'] = {
+        load() {
+            return null;
+        }}
 
-  function testServiceSetup() {
-    const calendarService: CalendarService = new CalendarService();
-    return { calendarService };
-  }
+    function testServiceSetup() {
+        const calendarService: CalendarService = new CalendarService();
+        return { calendarService };
+    }
 
-  it('should be created', () => {
-    const { calendarService } = testServiceSetup();
-    expect(calendarService).toBeTruthy();
-  });
-
-  describe('getAuth', () => {
-    
-    it('It should authenticate and initialize the client', () => {
-      const { calendarService } = testServiceSetup();
-      expect(calendarService.getAuth).toBeTruthy(); 
+    it('should be created', () => {
+        const { calendarService } = testServiceSetup();
+        expect(calendarService).toBeTruthy();
     });
-  });
 
-  describe('updateSigninStatus', () => {
-    //If email has not yet been received from gapi
-    it('Should return update status of false', () => {
-      const { calendarService } = testServiceSetup();
-      expect(calendarService.updateSigninStatus(false)).toBeFalsy();
+    describe('getAuth', () => {
+        it('It should authenticate and initialize the client', () => {
+            const { calendarService } = testServiceSetup();
+            expect(calendarService.getAuth).toBeTruthy(); 
+        });
+    });
+
+    describe('updateSigninStatus', () => {
+        //If email has not yet been received from gapi
+        it('Should return update status of false', () => {
+            const { calendarService } = testServiceSetup();
+            expect(calendarService.updateSigninStatus(false)).toBeFalsy();
+        })
+        //If email has been received from gapi
+        it('Should return update status of true', () => {
+            const { calendarService } = testServiceSetup();
+            calendarService.getAuth();
+            expect(calendarService.updateSigninStatus(true)).toEqual(undefined);
+        })
     })
-    //If email has been received from gapi
-    it('Should return update status of true', () => {
-      const { calendarService } = testServiceSetup();
-      calendarService.getAuth();
-      expect(calendarService.updateSigninStatus(true)).toEqual(undefined);
-    })
-
-  })
-
-
 });
 
 
--- src/app/core/services/calendar.service.ts
+++ src/app/core/services/calendar.service.ts
@@ -15,64 +15,55 @@ export class CalendarService {
 
     constructor() { }
 
-  getAuth() : boolean{   
-     gapi.load('client:auth2', ()=>{
-     gapi.client.init({
-        apiKey: '<API_KEY>',
-        clientId: '<CLIENT_ID>',
-        discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest'],
-        scope: 'https://www.googleapis.com/auth/calendar.readonly'
-      })
-    
-      .then(() => {
-        
-        // Prompt sign in
-        gapi.auth2.getAuthInstance().signIn();
+    getAuth() : boolean{   
+        gapi.load('client:auth2', ()=>{
+            gapi.client.init({
+                apiKey: '<API_KEY>',
+                clientId: '<CLIENT_ID>',
+                discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest'],
+                scope: 'https://www.googleapis.com/auth/calendar.readonly'
+              }).then(() => { 
+                  // Prompt sign in
+                  gapi.auth2.getAuthInstance().signIn();
 
-        // Set up inital listener on sign in state
-        gapi.auth2.getAuthInstance().isSignedIn.listen(this.updateSigninStatus);
+                  // Set up inital listener on sign in state
+                  gapi.auth2.getAuthInstance().isSignedIn.listen(this.updateSigninStatus);
 
-        // Update sign in status upon sign in
-        this.updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-        
-      }).catch(error =>{
-        console.log(error);
-      });
-    })
-    //client loaded and initialized (not necessarily signed in)
-    return true
-  }
+                  // Update sign in status upon sign in
+                  this.updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+              
+              }).catch(error =>{
+                  console.log(error);
+              });
+          })
+          //client loaded and initialized (not necessarily signed in)
+          return true
+    }
 
-  updateSigninStatus(isSignedIn) : boolean {  
-      this.signedIn = isSignedIn;
-      // Just testing out getting events from the calendar
-      try{
-      
-        if(this.signedIn){
-          this.email = gapi.auth2.getAuthInstance().currentUser.get().getBasicProfile().getEmail();
-          this.picture = gapi.auth2.getAuthInstance().currentUser.get().getBasicProfile().getImageUrl();
-          this.emailUpdatedSource.next();
-          return true;  
+    updateSigninStatus(isSignedIn) : boolean {  
+        this.signedIn = isSignedIn;
+        // Just testing out getting events from the calendar
+        try{
+            if(this.signedIn){
+              this.email = gapi.auth2.getAuthInstance().currentUser.get().getBasicProfile().getEmail();
+              this.picture = gapi.auth2.getAuthInstance().currentUser.get().getBasicProfile().getImageUrl();
+              this.emailUpdatedSource.next();
+              return true;  
+            }else{
+              return false;  
+            }
         }
-        else
-         return false;  
-      }
-      catch{
-        console.log('cannot log auth instance');
-      }     
-  }
-  
-  getUserEmail() {
-    return this.email;
-  }
+        catch{
+            console.log('cannot log auth instance');
+        }     
+    }
+    
+    getUserEmail(): string {
+        return this.email;
+    }
 
-  getUserPicture() {
-    return this.picture;
-  }
+    getUserPicture(): string {
+        return this.picture;
+    }
 }
 
-//jons client ID
-// 285078470897-kq3dv01araf42l6m28bquflo3upbdgn3.apps.googleusercontent.com
-
-//jons api key
-// AIzaSyDw0wQwrQmCGX0Oor2I8D4zgbItoiHowmw
