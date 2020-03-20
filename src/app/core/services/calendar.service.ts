1f457b1427984adf15658366e75cd9ce1e934910
JonathanHypex <jonathan@hypex.gg>
2020-03-20 19:32:37 -0400

    More formatting fixes, comments, ready for PR

--- src/app/core/services/calendar.service.ts
+++ src/app/core/services/calendar.service.ts
@@ -15,66 +15,76 @@ export class CalendarService {
 
     constructor() { }
 
-  getAuth() : boolean{   
-     gapi.load('client:auth2', ()=>{
-     gapi.client.init({
-        apiKey: 'AIzaSyBJTreK-eH9Y0gsoaStxOmj-ks4hnEBxLo',
-        clientId: '194605385874-lf5td1vtbahaa55dhe3eokp2g3o5ss88.apps.googleusercontent.com',
-        discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest'],
-        scope: 'https://www.googleapis.com/auth/calendar.readonly'
-      })
-    
-      .then(() => {
-        
-        // Prompt sign in
-        gapi.auth2.getAuthInstance().signIn()
-        
-        .then(() => {
+  
+    /**
+     * Load google client using OAuth2, prompt sign in
+     * and set up listener to run updateSignInStatus on user
+     * sign in
+     */
+    getAuth() : boolean{   
+        gapi.load('client:auth2', ()=>{
+        gapi.client.init({
+            apiKey: '<API_KEY>',
+            clientId: '<CLIENT_ID>',
+            discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest'],
+            scope: 'https://www.googleapis.com/auth/calendar.readonly'
+          })
+          .then(() => {
+              // Prompt sign in
+              gapi.auth2.getAuthInstance().signIn()
+              .then(() => {
+                    // Set up inital listener on sign in state
+                    gapi.auth2.getAuthInstance().isSignedIn.listen(this.updateSigninStatus);
 
-                  // Set up inital listener on sign in state
-                  gapi.auth2.getAuthInstance().isSignedIn.listen(this.updateSigninStatus);
+                    // Update sign in status upon sign in, update email 
+                    this.updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+                    this.emailUpdatedSource.next();
+                })
+                .catch(error =>{
+                    console.log(error); //debug
+                });
 
-        // Update sign in status upon sign in
-        this.updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+          }).catch(error =>{
+              console.log(error); //debug
+          });
+        })
+        //client loaded and initialized (not necessarily signed in)
+        return true
+    }
 
-        this.emailUpdatedSource.next();
+    /**
+     * Triggered when a change in sign in status occurs
+     * in the google client. Populates email in 
+     * sidemenu component
+     * @param isSignedIn boolean indicating whether user has 
+     * Returns true if signed in
+     */
+    updateSigninStatus(isSignedIn) : boolean {  
+        try{
+            this.signedIn = isSignedIn;
 
-        }).catch(error =>{
-        console.log(error);
-        });
-      }).catch(error =>{
-        console.log(error);
-      });
-    })
-    //client loaded and initialized (not necessarily signed in)
-    return true
-  }
-
-  updateSigninStatus(isSignedIn) : boolean {  
-
-      // Just testing out getting events from the calendar
-      try{
-
-        this.signedIn = isSignedIn;
-
-        if(this.signedIn){
-          this.email = gapi.auth2.getAuthInstance().currentUser.get().getBasicProfile().getEmail();
-          this.picture = gapi.auth2.getAuthInstance().currentUser.get().getBasicProfile().getImageUrl();
-          
-          return true;  
+            if(this.signedIn){
+                this.email = gapi.auth2.getAuthInstance().currentUser.get().getBasicProfile().getEmail();
+                this.picture = gapi.auth2.getAuthInstance().currentUser.get().getBasicProfile().getImageUrl();
+                return true;  
+            }
         }
-      }
-      catch{
-            console.log('cannot log auth instance');
-      }     
-  }
-    
-  getUserEmail(): string {
+        catch{
+            console.log('Auth instance not validated yet.'); //debug
+        }     
+    }
+    /**
+     * Accessor for email
+     */
+    getUserEmail(): string {
         return this.email;
-  }
+    }
 
-  getUserPicture(): string {
+    /**
+     * Accessor for user google display image
+     */
+    getUserPicture(): string {
         return this.picture;
-  }
+    }
 }
 
