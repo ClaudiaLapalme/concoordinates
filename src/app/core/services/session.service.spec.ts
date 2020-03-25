8af2e9a96bfc4856baf6af52daf1cd93a23c50e3
Lara <larat248@gmail.com>
2020-03-25 12:38:04 -0400

    #27 Added tests - added tests to session service - added tests to routes page

--- src/app/core/services/session.service.spec.ts
+++ src/app/core/services/session.service.spec.ts
@@ -3,10 +3,37 @@ import { TestBed } from '@angular/core/testing';
 import { SessionService } from './session.service';
 
 describe('SessionService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
 
-  it('should be created', () => {
-    const service: SessionService = TestBed.get(SessionService);
-    expect(service).toBeTruthy();
-  });
+    let service: SessionService;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.get(SessionService);
+
+    });
+
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
+
+    class MockMaps extends google.maps.Map {
+    }
+
+    it('should store the map', () => {
+
+        const mockMap = new MockMaps(null);
+        service.storeMapRef(mockMap);
+        expect(service.map).not.toBeNull();
+
+    });
+
+    it('should return the map', () => {
+
+        const mockMap = new MockMaps(null);
+        service.storeMapRef(mockMap);
+        const returnedMap = service.getMapRef();
+        expect(service.map).toEqual(returnedMap);
+
+    });
+
 });
--- src/app/routes/routes.page.spec.ts
+++ src/app/routes/routes.page.spec.ts
@@ -5,6 +5,8 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { IonicModule } from '@ionic/angular';
 import { CoreModule, TransportMode } from '../core';
 import { RoutesPage } from './routes.page';
+import { SearchComponent } from '../core/components';
+import { By } from '@angular/platform-browser';
 
 describe('RoutesPage', () => {
     let component: RoutesPage;
@@ -46,9 +48,64 @@ describe('RoutesPage', () => {
         const actualTime = component.form.controls.time.value.split(':');
         parsedDate.setHours(actualTime[0]);
         parsedDate.setMinutes(actualTime[1]);
-        
+
         await mockedComponent.getRoutes();
         expect(mockFactory.generateDefaultRoutes).toHaveBeenCalled();
+
+    });
+
+    describe('search', () => {
+
+        beforeEach(async(() => {
+            spyOn(component, 'setFrom').and.callThrough();
+            spyOn(component, 'setTo').and.callThrough();
+        }));
+
+        class MockPlaceResult implements google.maps.places.PlaceResult {
+            address_components?: google.maps.GeocoderAddressComponent[];
+            adr_address?: string;
+            aspects?: google.maps.places.PlaceAspectRating[];
+            formatted_address?: string;
+            formatted_phone_number?: string;
+            geometry?: google.maps.places.PlaceGeometry;
+            html_attributions?: string[];
+            icon?: string;
+            id?: string;
+            international_phone_number?: string;
+            name: string;
+            opening_hours?: google.maps.places.OpeningHours;
+            permanently_closed?: boolean;
+            photos?: google.maps.places.PlacePhoto[];
+            place_id?: string;
+            plus_code?: google.maps.places.PlacePlusCode;
+            price_level?: number;
+            rating?: number;
+            reviews?: google.maps.places.PlaceReview[];
+            types?: string[];
+            url?: string;
+            user_ratings_total?: number;
+            utc_offset?: number;
+            utc_offset_minutes?: number;
+            vicinity?: string;
+            website?: string;
+        }
+
+
+        it('should call setFrom when place selection is emitted', () => {
+            const search = fixture.debugElement.query(By.css('.search-from'));
+            const searchComponent = search.componentInstance;
+            const mockResult = new MockPlaceResult();
+            searchComponent.placeSelection.emit(mockResult);
+            expect(component.setFrom).toHaveBeenCalledWith(mockResult);
+        });
+        it('should call setTo when place selection is emitted', () => {
+            const search = fixture.debugElement.query(By.css('.search-to'));
+            const searchComponent = search.componentInstance;
+            const mockResult = new MockPlaceResult();
+            searchComponent.placeSelection.emit(mockResult);
+            expect(component.setTo).toHaveBeenCalledWith(mockResult);
+        });
+
 
     });
 
