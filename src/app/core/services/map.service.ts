3b893fc1912fa40c6f70c6940c04d87bca8e5d84
Lara <larat248@gmail.com>
2020-04-08 12:28:48 -0400

    #16 Rendered shuttle route - Modified shuttle route data - Rendered route on map specific to shuttle when clicking on shuttle route option

--- src/app/core/services/map.service.ts
+++ src/app/core/services/map.service.ts
@@ -5,6 +5,7 @@ import { Building, IndoorMap, IndoorRoute, Map, OutdoorMap, OutdoorRoute, Route 
 import { GoogleApisService } from './google-apis.service';
 import { LocationService } from './location.service';
 import { PlaceService } from './place.service';
+import { ShuttleService } from './shuttle.service';
 
 @Injectable()
 export class MapService {
@@ -14,7 +15,8 @@ export class MapService {
         private locationService: LocationService,
         private googleApis: GoogleApisService,
         private placeService: PlaceService,
-        private abstractPOIFactoryService: AbstractPOIFactoryService
+        private abstractPOIFactoryService: AbstractPOIFactoryService,
+        private shuttleService: ShuttleService
     ) {
         this.loadOutdoorMap();
     }
@@ -109,8 +111,8 @@ export class MapService {
      * Right now, this function only loads the indoors maps for three floors
      * of the H building (1,8,9).
      */
-    public loadIndoorMaps():  Record<number, IndoorMap> {
-        const floors = [1,8,9];
+    public loadIndoorMaps(): Record<number, IndoorMap> {
+        const floors = [1, 8, 9];
         const indoorMapFactory = this.abstractPOIFactoryService.createIndoorPOIFactory();
         let indoorMaps: Record<number, IndoorMap> = {};
 
@@ -133,8 +135,8 @@ export class MapService {
         }
     }
 
-    private createIndoorPOIsLabels (mapRef: google.maps.Map<Element>): void {
-        const hBuilding = <Building> this.outdoorMap.getPOI('Henry F. Hall Building');
+    private createIndoorPOIsLabels(mapRef: google.maps.Map<Element>): void {
+        const hBuilding = <Building>this.outdoorMap.getPOI('Henry F. Hall Building');
         const indoorMaps = hBuilding.getIndoorMaps();
 
         for (const floorNumber in indoorMaps) {
@@ -193,20 +195,19 @@ export class MapService {
     }
 
     displayRoute(map: google.maps.Map, route: Route) {
-        if ( route instanceof OutdoorRoute) {
+        if (route instanceof OutdoorRoute) {
             this.displayOutdoorRoute(map, route);
         } else if (route instanceof IndoorRoute) {
             // TODO: Render indoor route here
             return;
         }
-
-
     }
 
-    private displayOutdoorRoute(map: google.maps.Map, route: OutdoorRoute){
+    private displayOutdoorRoute(map: google.maps.Map, route: OutdoorRoute) {
         const renderer = this.getMapRenderer();
         renderer.setMap(map);
-        this.googleApis
+        if (!this.shuttleService.displayShuttleRoute(map, route)) {
+            this.googleApis
             .getDirectionsService()
             .route(route.getDirectionsRequestFromRoute(), (res, status) => {
                 if (status === 'OK') {
@@ -215,7 +216,9 @@ export class MapService {
                     console.log('Directions request failed due to ' + status);
                 }
             });
+        }
     }
+
     getMapRenderer(): google.maps.DirectionsRenderer {
         return this.googleApis.getMapRenderer();
     }
--- src/app/core/services/shuttle.service.ts
+++ src/app/core/services/shuttle.service.ts
@@ -1,21 +1,28 @@
 import { Injectable } from '@angular/core';
 import { Coordinates, Route, OutdoorRoute, RouteStep, TransportMode, Transport } from '../models';
 import { CampusBoundsService } from './campus-bounds.service';
+import { GoogleApisService } from './google-apis.service';
+import ShuttleRoute from '../data/shuttle-route.json';
 
 @Injectable({
     providedIn: 'root'
 })
 export class ShuttleService {
 
-    constructor(private campusBoundsService: CampusBoundsService) { }
+    constructor(private campusBoundsService: CampusBoundsService, private googleApisService: GoogleApisService) { }
 
     startCampus: string;
+
+    icon: google.maps.Icon = {
+        url: '../../../assets/icon/place_marker.svg',
+        scaledSize: new google.maps.Size(30, 30) // scaled size
+    };
 
     private isEligibleForShuttle(fromLocation: Coordinates, toLocation: Coordinates): boolean {
         if (this.campusBoundsService.isWithinBoundsOfLoyola(fromLocation) || this.campusBoundsService.isWithinBoundsOfSGW(fromLocation)) {
             if (this.campusBoundsService.isWithinBoundsOfLoyola(fromLocation)) {
                 if (this.campusBoundsService.isWithinBoundsOfSGW(toLocation)) {
-                    this.startCampus = 'Loyola';
+                    this.startCampus = 'LOY';
                     return true;
                 }
             } else if (this.campusBoundsService.isWithinBoundsOfSGW(fromLocation)) {
@@ -30,10 +37,9 @@ export class ShuttleService {
         return false;
     }
 
-    //TODO: Check start campus, import data from json and input the coordinates for the path in route step 
 
     generateShuttleRoute(
-        startLocation: google.maps.places.PlaceResult, 
+        startLocation: google.maps.places.PlaceResult,
         endLocation: google.maps.places.PlaceResult,
         routes: Route[]): Route[] {
         const startCoord = new Coordinates(startLocation.geometry.location.lat(), startLocation.geometry.location.lng(), null);
@@ -41,11 +47,59 @@ export class ShuttleService {
         const isEligible = this.isEligibleForShuttle(startCoord, endCoord);
 
         if (isEligible) {
-            const routeSteps = [new RouteStep(0, startCoord, endCoord, [startCoord, endCoord], 30, 'Take the shuttle', new Transport(0, 0,TransportMode.SHUTTLE, null))];
-            routes[routes.length-1] = new OutdoorRoute (startCoord, endCoord, null, null, null, routeSteps);
+            const shuttlePath = this.generateShuttlePath();
+            const routeSteps = [new RouteStep(0, startCoord, endCoord, shuttlePath, 30, 'Take the shuttle', new Transport(0, 0, TransportMode.SHUTTLE, null))];
+            routes[routes.length - 1] = new OutdoorRoute(startCoord, endCoord, null, null, null, routeSteps);
         }
-
         return routes;
+    }
 
+    generateShuttlePath(): Coordinates[] {
+        let path = [];
+        ShuttleRoute[this.startCampus].forEach(node => {
+            path.push(new Coordinates(parseFloat(node.lat), parseFloat(node.lng), null))
+        });
+        return path;
+    }
+
+
+    displayShuttleRoute(map: google.maps.Map, route: OutdoorRoute): boolean {
+        if (this.isShuttleRoute(route)) {
+            let path: google.maps.LatLng[] = [];
+            route.routeSteps.forEach(step => {
+                let pathCoord: google.maps.LatLng[] = [];
+                step.path.forEach(coord => {
+                    pathCoord.push(this.googleApisService.createLatLng(coord.getLatitude(), coord.getLongitude()));
+                });
+                path = path.concat(pathCoord);
+            });
+            console.log(path);
+
+            const shuttlePath = new google.maps.Polyline({
+                path: path,
+                geodesic: true,
+                strokeColor: '#000000',
+                strokeOpacity: 1.0,
+                strokeWeight: 2
+            });
+            shuttlePath.setMap(map);
+            map.setZoom(13);
+            map.setCenter(path[0]);
+            this.googleApisService.createMarker(path[path.length-1], map, this.icon);
+
+            return true;
+        }
+        return false;
+    }
+
+    isShuttleRoute(route: OutdoorRoute): boolean {
+        let isShuttle = false;
+
+        route.routeSteps.forEach(step => {
+            if (step.transport.travelType === 'SHUTTLE') {
+                isShuttle = true;
+            }
+        });
+        return isShuttle;
     }
 }
